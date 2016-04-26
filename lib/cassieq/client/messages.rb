010ce930cbc3065909cd0b5890113b35f088ba4e
Tom Conroy <tbconroy@gmail.com>
2016-04-25 20:14:23 -0400

    Use non-deprecated parameter

--- lib/cassieq/client/messages.rb
+++ lib/cassieq/client/messages.rb
@@ -3,13 +3,13 @@ require "json"
 module Cassieq
   class Client
     module Messages
-      def publish_message(queue_name, message, initial_invis_time = nil)
-        query = { initialInvisibilityTime: initial_invis_time } unless initial_invis_time.nil?
+      def publish_message(queue_name, message, initial_invis_seconds = nil)
+        query = { initialInvisibilitySeconds: initial_invis_seconds } unless initial_invis_seconds.nil?
         request(:post, "queues/#{queue_name}/messages", message, query)
       end
 
-      def next_message(queue_name, initial_invis_time = nil)
-        query = { initialInvisibilityTime: initial_invis_time } unless initial_invis_time.nil?
+      def next_message(queue_name, initial_invis_seconds = nil)
+        query = { initialInvisibilitySeconds: initial_invis_seconds } unless initial_invis_seconds.nil?
         request(:get, "queues/#{queue_name}/messages/next", nil, query)
       end
 
